cf2af5ec7e1bae1597820abcaaa6c9689c29eda7
JoeyLwq <39552464+JoeyLwq@users.noreply.github.com>
2020-01-14 18:40:12 +0800

    update

--- import_export/src/main.js
+++ import_export/src/main.js
@@ -1,5 +1,9 @@
 
 import {test1, var1, TestClass1} from './js/output.js'
+import css from './css/normal.css'
+//require('./css/normal.css')
+
+
 
 test1('try sth')
 
@@ -7,3 +11,29 @@ var class1 = new TestClass1('clazz1','18')
 
 console.log(class1)
 class1.func1()
+
+
+/*使用vue方案一*/
+import Vue from 'vue'
+
+//const app = new Vue({
+//    el: '#app',
+//    template: `
+//        <div id="app">{{message}}</div>
+//    `,
+//
+//    data: {
+//        message: 'vue集成webpack'
+//    }
+//})
+
+import App from './vue/final.vue'
+
+new Vue({
+    el: '#app',
+    template: '<App/>',
+    components: { App }
+
+
+})
+
--- import_export/webpack.config.js
+++ import_export/webpack.config.js
@@ -1,10 +1,14 @@
 const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const UglifyPlugin = require('uglifyjs-webpack-plugin')
 
 module.exports = {
     entry: './src/main.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: "bundle.js"
+        filename: 'bundle.js',
+//        publicPath: 'dist/'
     },
     module: {
         rules: [
@@ -13,7 +17,46 @@ module.exports = {
                 /*css-loader只负责加载，style-loader负责将样式添加到DOM中*/
                 /*使用多个loader时，从右向左读取*/
                 use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(png|jpg|gif)$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            /*当图片小于limit时，会被编译为base64格式，而大于时,将由file-loader负责编译，即需要安装file-loader*/
+                            limit: 5000,
+                            name: 'img/[name].[hash:8].[ext]'
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.vue$/,
+                use: [
+                    {
+                        loader: 'vue-loader',
+                    }
+                ]
             }
         ]
+    },
+    resolve: {
+        alias: {
+            /*指定我们使用的vue的版本*/
+            'vue$': 'vue/dist/vue.esm.js'
+        }
+    },
+    plugins: [
+        new webpack.BannerPlugin('Author: JoeyLin'),
+        new HtmlWebpackPlugin({
+            template: 'index.html'
+        }),
+        new UglifyPlugin()
+    ],
+    devServer: {
+        contentBase: './dist',
+        /*是否热更新*/
+        inline: true
     }
 }
